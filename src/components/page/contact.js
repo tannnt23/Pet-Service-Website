a9cfe88792b81f9b59ea3151cf130f301ce9154e
Devbeee <tngb1002@gmail.com>
2023-04-27 21:09:13 +0700

    update form contact

--- src/components/page/contact.js
+++ src/components/page/contact.js
@@ -1,101 +1,175 @@
 import Footer from "../footer";
 import Header from "../header";
+import { Component } from "react";
 import { FaStore } from 'react-icons/fa';
 import { FaPhoneAlt } from 'react-icons/fa';
 import { FaEnvelope } from 'react-icons/fa';
-import { HandleForm } from './handleEvent/handleForm'
-function Contact() {
-    return (   
-        <div>    
-            <Header />
-            <div className="Contact">
-                {/* Ảnh */}
-                {/* <section>
-                    <img src="https://th.bing.com/th/id/OIP.JATxhtdF75-57WzxAn8plAHaCK?pid=ImgDet&w=1280&h=375&rs=1" alt=""></img>
-                </section> */}
-                {/* chữ và form */}
-                <div className="flex mx-[156px] mt-[50px]">
-                    {/* chữ */}
-                    <div className="mr-[70px]">
-                        <div className="mb-12">
-                            <p className="mb-4">Liên Hệ</p>
-                            <h1 className="text-5xl font-bold text-[#49b3f4] ml-[-3px]">
-                                <div className="mb-2">Hãy ghé thăm</div> <span className="">Office Spa</span> nhé
-                            </h1>
-                        </div>
-                        <div className="mb-12">
-                            <p>
-                                <span className="font-semibold">Pet Service HCM</span> ra đời với mong muốn mang lại cho khách hàng sự chuyên nghiệp, uy tín mang nét đẹp hoa mỹ mà chúng tôi đem lại sự trải nghiệm tốt nhất cho thú cưng của mọi nhà. Với hơn nhiều năm kinh nghiệm trong ngành dịch vụ thú cưng bao gồm: Spa thú cưng, Thú y, Sản phẩm dành cho thú cưng, Khách sạn thú cưng, Dịch vụ dắt chó đi dạo, Cung cấp các dòng thú cưng chuyên nghiệp…
-                            </p>
-                        </div>
-                        <div className="transition-colors">
-                            <a className="flex items-center mb-4" target="_blank" href="https://www.google.com/maps/place/Tr%C6%B0%E1%BB%9Dng+%C4%90%E1%BA%A1i+h%E1%BB%8Dc+C%C3%B4ng+ngh%E1%BB%87+Th%C3%B4ng+tin+-+%C4%90HQG+TP.HCM/@10.8702143,106.8001841,17z/data=!4m6!3m5!1s0x317527587e9ad5bf:0xafa66f9c8be3c91!8m2!3d10.8700089!4d106.8030541!16s%2Fm%2F02qqlmm ">  
-                                <i><FaStore/></i>
-                                <p className="ml-1.5 hover:text-[#7787ea] transition-colors">Số 73 Số 49, Tân Quy, Quận 7, Thành phố Hồ Chí Minh</p>
-                            </a>
-                            <a className="flex items-center mb-4" href="tel: +0898 520 760">
-                                <i><FaPhoneAlt/></i>
-                                <p className="ml-1.5 hover:text-[#7787ea] transition-colors">Hotline 0898 520 760</p>
-                            </a>
-                            <a className="flex items-center mb-4" href="mailto:hi.petservicehcm@gmail.com">
-                                <i><FaEnvelope/></i>
-                                <p className="ml-1.5 hover:text-[#7787ea] transition-colors">hi.petservicehcm@gmail.com</p>
-                            </a>
-                        </div>
-
-                    </div>
-                    {/* form ý kiến */}
-                    <div>
+class Contact extends Component {
+    constructor (props) {
+        super(props);
+    }
+    onBlurEvent = () => {
+        this.Validator(
+            {
+                form: '#form-1',
+                rules: [
+                    this.isRequire('#fullName'),
+                    this.isRequire('#mess'),
+                    this.isEmail('#email')
+                ]
+            }
+        );
+    }
+    Validator = (options) => {
+        // Lấy ra form cần validate
+        var formElement = document.querySelector(options.form);
+        //Xử lý người dùng có nhập trường * hay ko 
+        // console.log(formElement)
+        function validate (inputElement, rule) {
+            var errorElement = inputElement.parentElement.querySelector('.form-message')
+            var errorMess = rule.test(inputElement.value);
+            if (errorMess) {
+                errorElement.innerText = errorMess;
+                inputElement.style.border = '2px solid red'
+            }
+            else {
+                errorElement.innerText = '';
+            }            
+        }
+        if(formElement)
+        {
+            options.rules.forEach(function(rule){
+                var inputElement = formElement.querySelector(rule.selector);
+                if (inputElement) {
+                    // Xử lý trường hợp blur khỏi input
+                    inputElement.onblur = function () {
+                        validate(inputElement, rule)
+                        console.log(123)
+                    }
+    
+                    // Xử lý khi bắt đầu nhập thì ẩn errorMess
+                    inputElement.oninput = function () {
+                        var errorElement = inputElement.parentElement.querySelector('.form-message')
+                        errorElement.innerText='';
+                        inputElement.style.border = '2px solid #7787ea'
                         
-                        <div>Đóng góp ý kiến</div>
-                        <div className="mb-3">Các trường được đánh dấu <span className="text-[#e80000] text-[18px] font-extrabold">*</span> là bắt buộc</div>
-                        
-                        <form  action="" method="POST" id="form-1">
-                            <div className="mb-[25px]">
-                                <label htmlFor="fullName" className="text-[18px] font-semibold">Họ Tên<span className="text-[#e80000] font-extrabold">*</span></label><br/>
-                                <input id="fullName" type="text" className="border-2 py-3 pr-80 pl-3 outline-none focus:border-[#7787ea]"></input>
-                                <div className="form-message text-[#e80000]"></div>
-
+                    }
+                }
+            })
+        }  
+        
+    }
+    isRequire = (selector) => {
+        return {
+            selector:selector,
+            test: function (value) {
+                return  value.trim() ? undefined : 'Vui lòng nhập trường này'
+            }
+        }
+    }
+    isEmail = (selector) => {
+        return {
+            selector:selector,
+            test:function(value){
+                var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+                return regex.test(value) ? undefined : 'Trường này phải là email'
+            },
+        }
+    }
+    render(){
+        return (   
+            <div>    
+                <Header />
+                <div className="Contact">
+                    {/* Ảnh */}
+                    {/* <section>
+                        <img src="https://th.bing.com/th/id/OIP.JATxhtdF75-57WzxAn8plAHaCK?pid=ImgDet&w=1280&h=375&rs=1" alt=""></img>
+                    </section> */}
+                    {/* chữ và form */}
+                    <div className="flex mx-[156px] mt-[50px]">
+                        {/* chữ */}
+                        <div className="mr-[70px]">
+                            <div className="mb-12">
+                                <p className="mb-4">Liên Hệ</p>
+                                <h1 className="text-5xl font-bold text-[#49b3f4] ml-[-3px]">
+                                    <div className="mb-2">Hãy ghé thăm</div> <span className="">Office Spa</span> nhé
+                                </h1>
                             </div>
-                            <div>
+                            <div className="mb-12">
+                                <p>
+                                    <span className="font-semibold">Pet Service HCM</span> ra đời với mong muốn mang lại cho khách hàng sự chuyên nghiệp, uy tín mang nét đẹp hoa mỹ mà chúng tôi đem lại sự trải nghiệm tốt nhất cho thú cưng của mọi nhà. Với hơn nhiều năm kinh nghiệm trong ngành dịch vụ thú cưng bao gồm: Spa thú cưng, Thú y, Sản phẩm dành cho thú cưng, Khách sạn thú cưng, Dịch vụ dắt chó đi dạo, Cung cấp các dòng thú cưng chuyên nghiệp…
+                                </p>
+                            </div>
+                            <div className="transition-colors">
+                                <a className="flex items-center mb-4" target="_blank" href="https://www.google.com/maps/place/Tr%C6%B0%E1%BB%9Dng+%C4%90%E1%BA%A1i+h%E1%BB%8Dc+C%C3%B4ng+ngh%E1%BB%87+Th%C3%B4ng+tin+-+%C4%90HQG+TP.HCM/@10.8702143,106.8001841,17z/data=!4m6!3m5!1s0x317527587e9ad5bf:0xafa66f9c8be3c91!8m2!3d10.8700089!4d106.8030541!16s%2Fm%2F02qqlmm ">  
+                                    <i><FaStore/></i>
+                                    <p className="ml-1.5 hover:text-[#7787ea] transition-colors">Số 73 Số 49, Tân Quy, Quận 7, Thành phố Hồ Chí Minh</p>
+                                </a>
+                                <a className="flex items-center mb-4" href="tel: +0898 520 760">
+                                    <i><FaPhoneAlt/></i>
+                                    <p className="ml-1.5 hover:text-[#7787ea] transition-colors">Hotline 0898 520 760</p>
+                                </a>
+                                <a className="flex items-center mb-4" href="mailto:hi.petservicehcm@gmail.com">
+                                    <i><FaEnvelope/></i>
+                                    <p className="ml-1.5 hover:text-[#7787ea] transition-colors">hi.petservicehcm@gmail.com</p>
+                                </a>
+                            </div>
+    
+                        </div>
+                        {/* form ý kiến */}
+                        <div>
+                            
+                            <div>Đóng góp ý kiến</div>
+                            <div className="mb-3">Các trường được đánh dấu <span className="text-[#e80000] text-[18px] font-extrabold">*</span> là bắt buộc</div>
+                            
+                            <form  action="" method="POST" id="form-1">
                                 <div className="mb-[25px]">
-                                    <label htmlFor="tel" className="text-[18px] font-semibold">Số Điện Thoại</label><br/>
-                                    <input id="tel" type="text" className="border-2 py-3 pr-80 pl-3 outline-none focus:border-[#7787ea]"></input>
-                                </div>
-                                <div className="mb-[25px]">
-                                    <label htmlFor="email" className="text-[18px] font-semibold">Email<span className="text-[#e80000] font-extrabold">*</span></label><br/>
-                                    <input id="email" type="email" className="border-2 py-3 pr-80 pl-3 outline-none focus:border-[#7787ea]"></input>
+                                    <label htmlFor="fullName" className="text-[18px] font-semibold">Họ Tên<span className="text-[#e80000] font-extrabold">*</span></label><br/>
+                                    <input id="fullName" type="text" onBlur={this.onBlurEvent} className="border-2 py-3 pr-80 pl-3 outline-none focus:border-[#7787ea]"></input>
                                     <div className="form-message text-[#e80000]"></div>
                                 </div>
-                            </div>
-                            <div className="mb-[25px]">
-                                <label htmlFor="mess" className="text-[18px] font-semibold">Lời Nhắn<span className="text-[#e80000] font-extrabold">*</span></label><br/>
-                                <textarea id="mess" className="w-full border-2 outline-none pl-3 h-[200px] focus:border-[#7787ea]" placeholder="PSH luôn lắng nghe mọi đóng góp quý báu và không ngừng nâng cấp cải thiện chất lượng dịch vụ nhằm phục vụ quý khách hàng một cách tốt nhất."></textarea>
-                                <div className="form-message text-[#e80000] mt-[-5px]"></div>
-                            </div>
-                            <div>
-                                <input value="Gửi yêu cầu" type="submit" className="border w-32 h-14 text-white bg-black hover:bg-[#c4c4c4] hover:cursor-pointer ease-in duration-100 transition-colors"></input>
-                            </div>
-                        </form>
+    
+                                <div>
+                                    <div className="mb-[25px]">
+                                        <label htmlFor="tel" className="text-[18px] font-semibold">Số Điện Thoại</label><br/>
+                                        <input id="tel" type="text" onBlur={this.onBlurEvent} className="border-2 py-3 pr-80 pl-3 outline-none focus:border-[#7787ea]"></input>
+                                    </div>
+                                    <div className="mb-[25px]">
+                                        <label htmlFor="email" className="text-[18px] font-semibold">Email<span className="text-[#e80000] font-extrabold">*</span></label><br/>
+                                        <input id="email" type="email" onBlur={this.onBlurEvent} className="border-2 py-3 pr-80 pl-3 outline-none focus:border-[#7787ea]"></input>
+                                        <div className="form-message text-[#e80000]"></div>
+                                    </div>
+                                </div>
+    
+                                <div className="mb-[25px]">
+                                    <label htmlFor="mess" className="text-[18px] font-semibold">Lời Nhắn<span className="text-[#e80000] font-extrabold">*</span></label><br/>
+                                    <textarea id="mess" className="w-full border-2 outline-none pl-3 h-[200px] focus:border-[#7787ea]" placeholder="PSH luôn lắng nghe mọi đóng góp quý báu và không ngừng nâng cấp cải thiện chất lượng dịch vụ nhằm phục vụ quý khách hàng một cách tốt nhất."></textarea>
+                                    <div className="form-message text-[#e80000] mt-[-5px]"></div>
+                                </div>
+    
+                                <div>
+                                    <input value="Gửi yêu cầu" type="submit" className="border w-32 h-14 text-white bg-black hover:bg-[#c4c4c4] hover:cursor-pointer ease-in duration-100 transition-colors"></input>
+                                </div>
+    
+                            </form>
+                        </div>
+                    </div>
+                    {/* google map */}
+                    <div className="mx-[156px] my-[75px]">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.2312404166914!2d106.80047917495881!3d10.870008889284517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527587e9ad5bf%3A0xafa66f9c8be3c91!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2jhu4cgVGjDtG5nIHRpbiAtIMSQSFFHIFRQLkhDTQ!5e0!3m2!1svi!2s!4v1682007015702!5m2!1svi!2s" width={1208} height={450} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                 </div>
-                {/* google map */}
-                <div className="mx-[156px] my-[75px]">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.2312404166914!2d106.80047917495881!3d10.870008889284517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317527587e9ad5bf%3A0xafa66f9c8be3c91!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2jhu4cgVGjDtG5nIHRpbiAtIMSQSFFHIFRQLkhDTQ!5e0!3m2!1svi!2s!4v1682007015702!5m2!1svi!2s" width={1208} height={450} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-            </div>
-            <HandleForm.Validator
-                form='#form-1'
-                rules = {[
-                    HandleForm.isRequire('#fullName'),
-                    HandleForm.isRequire('#mess'),
-                    HandleForm.isEmail('#email')
-                ]}
-            />
-            <Footer />
-            
-        </div>
-        
-    )
+                {/* <HandleForm.Validator
+                    form='#form-1'
+                    rules = {[
+                        HandleForm.isRequire('#fullName'),
+                        HandleForm.isRequire('#mess'),
+                        HandleForm.isEmail('#email')
+                    ]}
+                />*/}
+                <Footer />       
+            </div>    
+        )
+    }
 }
 export default Contact;
--- src/components/page/handleEvent/handleForm.js
+++ src/components/page/handleEvent/handleForm.js
@@ -1,9 +1,9 @@
-export const HandleForm = {
+const HandleForm = {
     Validator(options){
         // Lấy ra form cần validate
         var formElement = document.querySelector(options.form);
         //Xử lý người dùng có nhập trường * hay ko 
-        console.log(formElement)
+        // console.log(formElement)
         function validate (inputElement, rule) {
             var errorElement = inputElement.parentElement.querySelector('.form-message')
             var errorMess = rule.test(inputElement.value);
@@ -23,6 +23,7 @@ export const HandleForm = {
                     // Xử lý trường hợp blur khỏi input
                     inputElement.onblur = function () {
                         validate(inputElement, rule)
+                        console.log(123)
                     }
     
                     // Xử lý khi bắt đầu nhập thì ẩn errorMess
@@ -30,29 +31,17 @@ export const HandleForm = {
                         var errorElement = inputElement.parentElement.querySelector('.form-message')
                         errorElement.innerText='';
                         inputElement.style.border = '2px solid #7787ea'
+                        
                     }
                 }
             })
         }  
     },
-    isRequire(selector){
-        return {
-            selector:selector,
-            test: function (value) {
-                return  value.trim() ? undefined : 'Vui lòng nhập trường này'
-            }
-        }
-    },
-    isEmail(selector){
-        return {
-            selector:selector,
-            test:function(value){
-                var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-                return regex.test(value) ? undefined : 'Trường này phải là email'
-            },
-        }
-    }
-
+    
+   
 }
+export default HandleForm
+// export default function test () {
+//     console.log("Như nào")
+// }
 
-// export default HandleForm.Validator
